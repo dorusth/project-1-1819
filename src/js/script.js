bc287f1b0a75f4272867a330b742cb7ed57c45f9
dorusth <dorusth@hotmail.com>
2019-03-04 16:50:11 +0100

    added data request

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
+import API from './api/index.js';
 
+async function init(){
+	const api = new API();
+	const stream = await api.createStream("search/beatles&facet=type(cd)");
+	stream
+		.pipe(renderToDocument);
+}
+
+function renderToDocument(stream){
+	const el = document.querySelector('main');
+	console.log(stream);
+}
+init()
